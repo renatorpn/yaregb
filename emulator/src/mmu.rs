fdfbb62edfd5cb61a7f1735896b35f8ae040806a
renato nascimento <19597832+renatorpn@users.noreply.github.com>
2022-02-28 19:39:22 +0000

    added timer control and joypad handling

--- emulator/src/mmu.rs
+++ emulator/src/mmu.rs
@@ -76,33 +76,155 @@ impl Mmu{
 
     }
 
-}
-
-
-pub fn bus_read(&self, address:u16) -> u8{
-    let address;
-    if address < 0x8000 {
-        self.read_rom(address)
-    }else if address >= 0x8000 && address < 0xC000  {
-        self.read_ram(address)
-    }else {
-        self.memory[address as usize]
+    pub fn bus_read(&self, address:u16) -> u8{
+        let address;
+        if address < 0x8000 {
+            self.read_rom(address)
+        }else if address >= 0x8000 && address < 0xC000  {
+            self.read_ram(address)
+        }else {
+            self.memory[address as usize]
+        }
     }
-}
-
-
-// may seem strange, but some carts do have access to write to ROM (like lithium battery)
-pub fn bus_write(&self, address:u16 ) -> u8{
-    let address;
-    if address < 0x8000 {
-        self.write_rom(address)
-    }else if address >= 0x8000 && address < 0xC000  {
-        self.read_ram(address)
-    }else {
-        self.memory[address as usize]
+    pub fn reset(&mut self){
+        self.memory[0xFF05] = 0x00;
+        self.memory[0xFF06] = 0x00;
+        self.memory[0xFF07] = 0x00;
+        self.memory[0xFF10] = 0x00;
+        self.memory[0xFF11] = 0x00;
+        self.memory[0xFF12] = 0x00;
+        self.memory[0xFF14] = 0x00;
+        self.memory[0xFF16] = 0x00;
+        self.memory[0xFF17] = 0x00;
+        self.memory[0xFF19] = 0x00;
+        self.memory[0xFF1A] = 0x00;
+        self.memory[0xFF1B] = 0x00;
+        self.memory[0xFF1E] = 0x00;
+        self.memory[0xFF20] = 0x00;
+        self.memory[0xFF21] = 0x00;
+        self.memory[0xFF22] = 0x00;
+        self.memory[0xFF23] = 0x00;
+        self.memory[0xFF24] = 0x00;
+        self.memory[0xFF25] = 0x00;
+        self.memory[0xFF26] = 0x00;
+        self.memory[0xFF40] = 0x00;
+        self.memory[0xFF42] = 0x00;
+        self.memory[0xFF43] = 0x00;
+        self.memory[0xFF45] = 0x00;
+        self.memory[0xFF47] = 0x00;
+        self.memory[0xFF48] = 0x00;
+        self.memory[0xFF49] = 0x00;
+        self.memory[0xFF4A] = 0x00;
+        self.memory[0xFF4B] = 0x00;
+        self.memory[0xFFFF] = 0x00;
+    
+        self.memory[joypad_register_address as usize] = 0xFF;
+        self.load_rom();
+    
     }
+    
+    // may seem strange, but some carts do have access to write to ROM (like lithium battery)
+    pub fn bus_write(&self, address:u16 ) -> u8{
+        let address;
+        if address < 0x8000 {
+            self.write_rom(address)
+        }else if address >= 0x8000 && address < 0xC000  {
+            self.read_ram(address)
+        }else {
+            self.memory[address as usize]
+        }
+    }
+
+    fn load_rom(&mut self){
+        let end_address = 0x8000;
+        for i in 0..cmp::min(end_address, self.rom.lenght()){
+            self.memory[i] = self.rom.get_byte(i);
+        }
+        self.mbc = get_mbc(&self.rom)
+    }
+
+    fn read_rom(&self, addr: Word) -> Byte{
+        match &self.mbc{
+            Some(mbc) => mbc.read_rom(addr - 0xA000);
+            None => self.rom.get_byte(addr as usize),
+        }
+    }
+
+    fn read_ram(&self, addr: Word) -> Byte{
+        match &self.mbc{
+            Some(mbc) => mbc.read_ram(addr - AxA000, data),
+            None(mbc) => self.memory[addr as usize],
+        }
+    }
+
+    fn write_ram(&mut self, addr: Word, data: Byte){
+        match &mut self.mbc{
+            Some(mbc) => mbc.write_ram(addr - AxA000, data),
+            None(mbc) => self.memory[addr as usize] = data,
+        }
+    } 
+
+    fn handle_bank(&mut self, addr: Word, data: Byte){
+        match &mut self.mbc{
+            Some(mbc) => mbc.handle_bank(addr, data);
+            None => {},
+        }
+    }
+
+    //I don't understand this, why 0x3?
+    fn handle_joypad(&mut self, addr: Word, data: Byte){
+        let mode_bits = (data >> 4) & 0x3;
+        let mode = match mode_bits {
+            1 => Some(Joypad::ACTION), 
+            2 => Some(Joypad::DIRECTION),
+            _ => None
+        };
+
+        if let Some(joypad_mode) = mode{
+            let low_4bits = self.joypad.get_buttons_for_mode(joypad_mode);
+            self.memory[addr as usize] = data (data & 0xF0) | low_4bits;
+        }else{
+            self.memory[addr as usize] = data (data & 0xF0) | 0xF0;
+        }
+    }
+
+    pub fn update_timer_freq(&mut self, val:bool){
+        self.timer_freq_changed = val;
+    }
+
+    pub fn timer_freq_is_changed(&mut self) -> bool{
+        self.timer_freq_changed
+    }
+
+    pub fn update_scanline(&mut self){
+        self.memory[CURRENT_SCANLINE_ADDR as usize] = self.memory[CURRENT_SCANLINE_ADDR as usize].wrapping_add(1);
+    }
+
+    pub fn reset_scanline(&mut self){
+        self.memory[CURRENT_SCANLINE_ADDR as usize] = 0;
+    }
+
+    pub fn increment_timer_register(&mut self){
+        self.memory[TIMER_ADDR as usize] = self.memory[TIMER_ADDR as usize].wrapping_add(1);
+    }
+
+    pub fn increment_division_register(&mut self){
+        self.memory[DIVIDER_REGISTER_ADDR as usize] = self.memory[DIVIDER_REGISTER_ADDR as usize].wrapping_add(1);
+    }
+
+    pub fn set_button_state(&mut self, button:usize){
+        self.joypad.set_button_state(button);
+    }
+
+    pub fn reset_button_state(&mut self, button:usize){
+        self.joypad.reset_button_state(button);
+    }
+
+    fn timer_control(&mut self, data:Byte){
+        self.update_timer_freq(true);
+        self.memory[TIMER_CONTROL_ADDR as usize] = data;
+    }
+
 }
 
-pub fn reset(&mut self){
 
-}
